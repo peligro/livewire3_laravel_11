ce399ea189eceeac83c25e56debc87cc407c587d
peligro <yo@cesarcancino.com>
2024-03-15 16:17:56 -0300

    modificación archivo web para ruta update

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ Route::get('/eventos', EventosComponent::class)->name("ruta_eventos");
 Route::get('/formularios', FormulariosComponent::class)->name("ruta_formularios");
 //https://github.com/livewire/livewire/discussions/7480
 \Livewire\Livewire::setUpdateRoute(function ($handle) {
-    return Route::post('/clientes/tamila/test/mvp/livewire_mvp/public/livewire/update', $handle);
+    return Route::post('/pubic/livewire/update', $handle);
 });
